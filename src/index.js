693f4e5eaec2db28033bea075ca8359059fbe3be
Rishav Kumar <rishavkr000@gmail.com>
2022-04-22 17:58:17 +0530

    21th April assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://urajrishu:aUHDB96UyJaq9SB@cluster0.1wief.mongodb.net/rishavkr000", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
